4a5f34d7666bb09253af3c7d47f3acc0796d9b20
noyoungchul <shdudcjf107@gmail.com>
2025-09-04 14:53:37 +0900

    TDD 테스트

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,14 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//스프링 부트의 자동 설정, 스프링 brean 읽기와 생성을 모두 사용
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
---
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot;
+
+import com.jojoldu.book.springboot.web.HelloController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+// MVC(Spring MVC) 여러 스프링 테스트 어노테이션 중 web에 집중할 수 있는 어노테이션
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    // 스프링이 관리하는 빈(Bean)을 주입받습니다
+    @Autowired
+    // 웹 API를 테스트 할 때 사용합니다
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                //HTTP 헤더의 status를 검증합니다.
+                .andExpect(status().isOk())
+                // mvc.perform의 결과를 검증합니다.
+                .andExpect(content().string(hello));
+    }
+}
